8abde85035392a83fc0612d01ebf330f46b9e248
Larry <larry@ionlearning.com>
2023-11-20 21:43:09 -0600

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -17,7 +17,7 @@ alias ..="cd .."
 
 # Laravel
 alias art="php artisan"
-alias ams="php artisan migrate:fresh --seed"
+alias mfs="php artisan migrate:fresh --seed"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
@@ -54,3 +54,8 @@ alias gl="git log --oneline --decorate --color"
 alias gnuke="git clean -df && git reset --hard"
 alias nah="git reset --hard && git clean -df"
 alias wip="git add . && git commit -m 'wip'"
+
+# Gcloud
+alias playground="./cloud_sql_proxy -instances=ionplayground-344320:us-central1:ion-db-1=tcp:3307"
+alias qa="./cloud_sql_proxy -instances=ion-testing-350520:us-central1:ion-db-1=tcp:3308"
+alias prod="./cloud_sql_proxy -instances=ion-learn-production:us-central1:ion-db-e3abe04d=tcp:3309"
